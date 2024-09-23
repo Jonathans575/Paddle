85b8b834c19f950127498ab3841e0caaf6575a22
ming1753 <61511741+ming1753@users.noreply.github.com>
2024-09-23 22:00:00 +0800

    pull argsort fix

--- paddle/phi/kernels/gpu/argsort_kernel.cu
+++ paddle/phi/kernels/gpu/argsort_kernel.cu
@@ -251,12 +251,18 @@ void ArgsortKernel(const Context& dev_ctx,
   // Compared to the following 'Special case for full sort', ascending sort is
   // 34 times faster and descending sort is 31 times faster.
   if (size == in_dims[axis]) {
-    thrust::sequence(thrust::device, ids_data, ids_data + size);
-    thrust::copy(thrust::device, in_data, in_data + size, out_data);
-    thrust::sort_by_key(thrust::device, out_data, out_data + size, ids_data);
+#ifdef PADDLE_WITH_CUDA
+    const auto& exec_policy = thrust::cuda::par.on(dev_ctx.stream());
+#else
+    const auto& exec_policy = thrust::hip::par.on(dev_ctx.stream());
+#endif
+
+    thrust::sequence(exec_policy, ids_data, ids_data + size);
+    thrust::copy(exec_policy, in_data, in_data + size, out_data);
+    thrust::sort_by_key(exec_policy, out_data, out_data + size, ids_data);
     if (descending) {
-      thrust::reverse(thrust::device, out_data, out_data + size);
-      thrust::reverse(thrust::device, ids_data, ids_data + size);
+      thrust::reverse(exec_policy, out_data, out_data + size);
+      thrust::reverse(exec_policy, ids_data, ids_data + size);
     }
     return;
   }
